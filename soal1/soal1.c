25228eb09d4b9283f62075e14569d6d424e6eb47
Ghimnastiar Al Abiyyuna <75864703+whtprm@users.noreply.github.com>
2021-04-24 10:31:35 +0700

    Add files via upload

---
+++ soal1/soal1.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#include <wait.h>
+
+int main(){
+	
+	
+	
+}
